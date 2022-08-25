7cbb59b589ddc9015c6e1f6bb629745ed2e613b9
geunhyuk park <rmsgur95@gmail.com>
2022-08-25 14:03:51 +0900

    최종:오류수정 전 ver1

--- front/src/components/award/Award.js
+++ front/src/components/award/Award.js
@@ -1,25 +1,28 @@
 import React from "react";
 import AwardEditForm from "./AwardEditForm";
-
+import AwardCard from "./AwardCard";
 
 const Award=(award,setAwards,isEditable)=>{
     let [isEditing,setisEditing]= useState(false)
     return(
-        <>
-        {
-            isEditing ? (
-                <AwardEditForm
-                    currentAward={award}
-                    setAwards={setAwards}
-                    setisEditing={setisEditing}
-                />
-            )
-            :(
-               <></>
-                // 목록들
-            )
-        }
-        </>
+        <div>
+            {
+                isEditing ? (
+                    <AwardEditForm
+                        currentAward={award}
+                        setAwards={setAwards}
+                        setisEditing={setisEditing}
+                    />
+                )
+                :(
+                <AwardCard
+                        award={award}
+                        isEditable={isEditable}
+                        setisEditing={setisEditing}
+                    />
+                )
+            }
+        </div>
     )
 }
 
--- front/src/components/award/Awards.js
+++ front/src/components/award/Awards.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import { Card, Button, Row, Col } from "react-bootstrap";
 import * as Api from "../../api";
+import Award from './Award';
 import AwardAddForm from './AwardAddForm';
+
 const Awards=({portfolioOwnerId, isEditable})=>{
-    let [awards, setAwards]= useState([]);
+    let [awards, setAwards]= useState([])
     let [isAdding,setIsAdding]=useState(false);
 
     useEffect(()=>{
@@ -15,7 +17,14 @@ const Awards=({portfolioOwnerId, isEditable})=>{
         <Card>
             <Card.Body>
                 <Card.Title>수상이력</Card.Title>
-                {/* awards 맵으로 데이터 변환 */}
+                {awards.map((award)=>(
+                    <Award
+                        key={award.id}
+                        award={award}
+                        setAwards={setAwards}
+                        isEditab={isEditable}
+                    />
+                ))}
                 {isEditable && (
                     <Row className='mt-3 text-center mb-4'>
                         <Col sm={{span:20}}>
