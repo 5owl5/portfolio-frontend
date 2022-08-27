7cec13b867ff5ac6bbc7aabbf352870412abffad
현근 손 <sonners.sean@gmail.com>
2022-08-27 05:08:48 +0000

    Update EducationCard.js

--- front/src/components/education/EducationCard.js
+++ front/src/components/education/EducationCard.js
@@ -1,13 +1,17 @@
 import { Card, Button, Row, Col } from "react-bootstrap";
 
 function EducationCard({ education, isEditable, setIsEditing }) {
+  const awardDate = award.awardDate.split("T")[0];
+
   return (
     <Card.Text>
       <Row className="align-items-center">
         <Col>
-          <span>{education.title}</span>
+          <span>{education.educationTitle}</span>
           <br />
-          <span className="text-muted">{education.description}</span>
+          <span className="text-muted">{education.educationName}</span>
+          <br />
+          <span className="text-muted">{awardDate}</span>
         </Col>
         {isEditable && (
           <Col xs lg="1">
