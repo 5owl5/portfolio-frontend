70ab4879ff12f69087087fecd2a09135ba300067
zzzzsb <ji12subin@gmail.com>
2022-09-01 02:45:53 +0900

    Remove: 주석 삭제

--- front/src/atoms/theme.js
+++ front/src/atoms/theme.js
@@ -1,29 +1,5 @@
 import { atom } from "recoil";
 
-// // light mode
-// export const lightState = atom({
-//   key: "light",
-//   default: {
-//     mode: "light",
-//     bgColor: "#F8F9FA",
-//     bgColor2: "#ffffff",
-//     textColor: "#31302E",
-//     btnBorder: "1px solid #eaeaea",
-//   },
-// });
-
-// // dark mode
-// export const darkState = atom({
-//   key: "dark",
-//   default: {
-//     mode: "dark",
-//     bgColor: "#121212",
-//     bgColor2: "#1e1e1e",
-//     textColor: "#ECECEC",
-//     btnBorder: "0.5px solid #2c2d33",
-//   },
-// });
-
 // light mode
 export const lightMode = {
   mode: "light",
@@ -45,15 +21,6 @@ export const darkMode = {
 function getTheme() {
   const curTheme = window.localStorage.getItem("theme");
   return curTheme ? curTheme : "light";
-  // if (persistedColorPreference) {
-  //   return persistedColorPreference;
-  // }
-
-  // const systemPreference = window.matchMedia("(prefers-color-scheme: dark)");
-  // if (systemPreference.matches) {
-  //   return "dark";
-  // }
-  // return "light";
 }
 
 // 현재 mode 상태
