03560959defdadd3f5ec4c6ea971bdb58b141691
geunhyuk park <rmsgur95@gmail.com>
2022-09-01 20:50:53 +0900

    Fix: certificatecard 주석 삭제

--- front/src/components/certificate/CertificateCard.js
+++ front/src/components/certificate/CertificateCard.js
@@ -41,16 +41,6 @@ function CertificateCard({
       alert("오류가 발생했습니다", err);
     }
   };
-  //   try {
-  //     if (window.confirm("삭제하시겠습니까?")) {
-  //       await Api.delete(`cer/${currentCertificate._id}`);
-  //       const res = await Api.get(`users/${portfolioOwnerId}/cer`);
-  //       setCertificates(res.data);
-  //     }
-  //   } catch (err) {
-  //     alert("오류가 발생했습니다.", err);
-  //   }
-  // };
 
   return (
     <Row className="mb-4">
